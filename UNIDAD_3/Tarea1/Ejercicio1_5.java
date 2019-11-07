cae767c068e290a37a69e9f89edd89258fedb57a
erHorhe <55448904+erHorhe@users.noreply.github.com>
2019-11-07 19:07:30 +0100

    Update Ejercicio1_5.java

--- UNIDAD_3/Tarea1/Ejercicio1_5.java
+++ UNIDAD_3/Tarea1/Ejercicio1_5.java
@@ -1,30 +1,39 @@
-package Tarea1;
+package UNIDAD_3;
 
 import java.util.Scanner;
 
 public class Ejercicio1_5 {
 
-	static short decirNumMayor(short n1, short n2, short n3) {
+	static short decirNumMayor(short n1, short n2) {
+		
+		short resultadoMayor = 0;
 		if (n1 > n2) {
-			return n1;
+			
+			resultadoMayor = n1;
+		
 		} else if (n2>n1) {
-			return n2;
+			
+			resultadoMayor = n2;
+			
 		}
-		return n3;
+		
+		return resultadoMayor;
 	}
 	
 	public static void main(String[] args) {
 		
 		Scanner t = new Scanner(System.in);
 		
-		System.out.print("Primer número: ");
+		System.out.print("Primer nÃºmero: ");
 		short num1 = t.nextShort();
-		System.out.print("Segundo número: ");
+		System.out.print("Segundo nÃºmero: ");
 		short num2 = t.nextShort();
-		System.out.print("Tercer número: ");
+		System.out.print("Tercer nÃºmero: ");
 		short num3 = t.nextShort();
 		
-		System.out.println(decirNumMayor(num1, num2, num3) + " es el mayor número.");
+		short numMayor = decirNumMayor(num1,num2);
+		
+		System.out.println(decirNumMayor(numMayor, num3) + " es el mayor nÃºmero.");
 		
 		t.close();
 	}
